13a6f1b22b908f2bd60c457c2f57b46dd797ad05
Jem Young <jemuel.young@gmail.com>
2016-05-03 10:11:43 -0700

    wip

--- static/babel-worker.js
+++ static/babel-worker.js
@@ -1,7 +1,17 @@
 importScripts('babel.min.js');
 
+/**
+ * Transpile a js file into ES5
+ * @param  {String} input The file to transpile
+ * @return {String}       The transpiled file
+ */
 const transform = (input) => Babel.transform(input, { presets: ['es2015'] }).code;
 
+/**
+ * Test of the requested object is a javascript file
+ * @param  {Request} request The request
+ * @return {Boolean}         True if the object a javascript file
+ */
 const isRequestJS = (request) => {
   if (request && request.url) {
     const url = request.url;
--- static/worker.js
+++ static/worker.js
@@ -1,42 +1,37 @@
-// importScripts('socket.io.js');
 
-
-this.addEventListener('install', (event) => {
+this.oninstall = (event) => {
   event.waitUntil(
     caches.open('v1')
       .then((cache) => {
         return cache.addAll([
-          '/index.html'
+          '/',
+          '/index.html',
+          '/images/cat1.jpg',
+          '/images/cat2.jpg',
+          '/images/cat3.jpg',
+          '/images/cat4.jpg',
         ]);
-      })
-      .then(()=>{ console.log('service worker installed')})
-      .then(()=> self.skipWaiting())
+      }).then(self.skipWaiting)
   );
-});
+};
+
 
 this.addEventListener('activate',  () => self.clients.claim());
 
-this.addEventListener('fetch', (event) => {
-  event.respondWith(
+this.onfetch = (event) => {
+  const response =
     caches.match(event.request)
-      .catch(() => {
-        return fetch(event.request);
-      })
-  );
-});
+      .catch(() =>
+        fetch(event.request)
+          .then((res) => {
+            const r = res.clone();
+            caches.open('v1').then((cache) => {
+              cache.put(event.request, r);
+            });
+          return res;
+          }))
+      .then((res) => res);
 
-
-  ws = new WebSocket("ws://localhost:8082");
-  ws.onopen = function() {
-    // the socket is open
-    ws.send("foo");
-  };
-  ws.onmessage = function (evt){
-    // message received
-    console.log(evt.data);
-  };
-  ws.onclose = function(e) {
-    // websocket is closed.
-    console.log('closed for', e);
-  };
+  event.respondWith(response);
+};
 
